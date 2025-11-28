b0071f108a0f8063a9d52def600762a12189ed99
Hitman-886 <bhaveshsanghavi21@gmail.com>
2025-11-28 19:46:12 +0530

    Update relational opt.c

--- relational opt.c
+++ relational opt.c
@@ -1,17 +1,19 @@
 # include <stdio.h>
 int main(){
 
-int a,b,c;
+int a,b;
 printf(" ENTER 2 VALUES: ");
-scanf("%d\n,%d\n",&a,&b);
+scanf("%d\n,%d\n",&a,&b);         //taking input from user
 
-c - (a==b);
-printf("\n Equal : %d ",c);
+printf("\n USING RELATIONAL OPERATORS :");
+printf("a==b : %d\n ", a==b);      // equal to 
+printf("a!=b : %d\n",a!=b);        // not equal to
+printf("a>b  : %d\n",a>b);         // greater than
+printf("a<b  : %d\n",a<b);         // less than
+printf("a>=b : %d\n",a>=b);       // greater than or equal to
+printf("a<=b : %d\n",a<=b);       // less than or equal to
 
-
-
-
-
+// all relational operators return either 1 (true) or 0 (false)
 
 
 return 0;
